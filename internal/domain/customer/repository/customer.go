ac21c1b051b301f03ce6d122e4e39a0432cb59db
André Leoni <andreluizleoni@gmail.com>
2022-12-14 15:28:57 -0300

    customer find dto handler

--- internal/domain/customer/repository/customer.go
+++ internal/domain/customer/repository/customer.go
@@ -3,7 +3,7 @@ package repository
 import "golang-clean-arch-reference/internal/domain/customer/entity"
 
 type Customer interface {
-	Get(ID string) (*entity.Customer, error)
+	Find(ID string) (*entity.Customer, error)
 	Create(*entity.Customer) error
 	Update(*entity.Customer) error
 	Delete(*entity.Customer) error
---
+++ internal/usecase/customer/find/handler.go
@@ -0,0 +1,21 @@
+package find
+
+import "golang-clean-arch-reference/internal/domain/customer/repository"
+
+type UseCaseCustomerFindHandler struct {
+	customerRepository repository.Customer
+}
+
+func (uccfh UseCaseCustomerFindHandler) Handle(icfd InputCustomerFindDTO) (OutputCustomerFindDTO, error) {
+	response := OutputCustomerFindDTO{}
+
+	result, err := uccfh.customerRepository.Find(icfd.ID)
+	if err == nil {
+		return response, err
+	}
+
+	response.ID = result.ID
+	response.Name = result.Name
+
+	return response, nil
+}
