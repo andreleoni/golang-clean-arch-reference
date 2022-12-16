2ae7117f9aaa6c5fe14e9120e504cf4e69f83c61
André Leoni <andreluizleoni@gmail.com>
2022-12-16 13:06:04 -0300

    handler integration update test

--- internal/usecase/order/update/handler_integration_test.go
+++ internal/usecase/order/update/handler_integration_test.go
@@ -1,41 +1,54 @@
 package update
 
 import (
-	"golang-clean-arch-reference/internal/domain/customer/entity"
+	"golang-clean-arch-reference/internal/domain/order/entity"
 	"golang-clean-arch-reference/internal/infraestructure/database/postgres"
-	"golang-clean-arch-reference/internal/infraestructure/persistence/customer"
+	orderpersistence "golang-clean-arch-reference/internal/infraestructure/persistence/order"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUseCaseCustomerUpdateHandler_Integration(t *testing.T) {
+func TestUseCaseOrderUpdateHandler_Integration(t *testing.T) {
 	postgres.PGSetup()
 
-	customerRepository := customer.NewCustomer(postgres.PG)
+	orderRepository := orderpersistence.NewOrder(postgres.PG)
 
-	expectedID := "my-uuid"
-	expectedName := "my name"
+	expectedID := "oder-uuid"
+	expectedProductID := "product-uuid-1"
+	expectedCustomerID := "customer-uuid-1"
+	expectedQuantity := uint64(2)
 
-	customer := entity.Customer{ID: expectedID, Name: expectedName}
+	order := entity.Order{
+		ProductID:  expectedProductID,
+		CustomerID: expectedCustomerID,
+		Quantity:   uint64(expectedQuantity),
+	}
 
-	err := customerRepository.Create(&customer)
+	err := orderRepository.Create(&order)
 	assert.Nil(t, err)
 
 	t.Run("when updating the record", func(t *testing.T) {
-		customerUpdateHandler := NewUseCaseCustomerUpdateHandler(customerRepository)
+		customerUpdateHandler := NewUseCaseOrderUpdateHandler(orderRepository)
 
-		myUpdatableName := "my another name"
+		myNewQuantity := uint64(4)
 
-		icfd := InputCustomerUpdateDTO{ID: expectedID, Name: myUpdatableName}
+		icfd := InputOrderUpdateDTO{
+			ID:         expectedID,
+			ProductID:  expectedProductID,
+			CustomerID: expectedCustomerID,
+			Quantity:   uint64(myNewQuantity),
+		}
 
 		result, err := customerUpdateHandler.Handle(icfd)
 		assert.Nil(t, err)
 
-		findResult, err := customerRepository.Find(result.ID)
-		assert.Nil(t, err)
+		findResult, err := orderRepository.Find(result.ID)
 
-		assert.Equal(t, result.ID, findResult.ID)
-		assert.Equal(t, result.Name, myUpdatableName)
+		assert.Nil(t, err)
+		assert.NotEqual(t, findResult.ID, "")
+		assert.Equal(t, findResult.ProductID, expectedProductID)
+		assert.Equal(t, findResult.CustomerID, expectedCustomerID)
+		assert.Equal(t, findResult.Quantity, uint64(2))
 	})
 }
